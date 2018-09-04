4d23ea3f3b25d1a172129495cb0a6fd75362e058
denchikUniversal <tkachenkodenys94@gmail.com>
2018-09-04 15:44:24 +0300

    material module theme.scss

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   imports: [
-    CommonModule
+    MatButtonModule
   ],
-  declarations: []
+  exports: [
+    MatButtonModule
+  ]
 })
 export class MaterialModule { }
