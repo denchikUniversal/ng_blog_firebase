c2afa9a2ede58f123b895f6d4f4ec2d79cb1cf22
denchikUniversal <tkachenkodenys94@gmail.com>
2018-09-04 16:09:57 +0300

    login and logout functions

--- src/app/core/auth.service.ts
+++ src/app/core/auth.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(public afAuth: AngularFireAuth) { }
+
+  login() {
+    this.afAuth.auth.signInWithPopup(new firebase.auth.GithubAuthProvider)
+  }
+
+  logout() {
+    this.afAuth.auth.signOut();
+  }
+
 }
