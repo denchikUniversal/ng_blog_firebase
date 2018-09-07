fff5171d98a1b1797f3fb023413af8784e5d06fc
denchikUniversal <tkachenkodenys94@gmail.com>
2018-09-07 12:24:04 +0300

    ngIf for content editing with ng-content

--- src/app/posts/post-detail/post-detail.component.ts
+++ src/app/posts/post-detail/post-detail.component.ts
@@ -29,6 +29,16 @@ export class PostDetailComponent implements OnInit {
     return this.postService.getPostData(id).subscribe( data => this.post = data)
   }
 
+  updatePost() {
+    const formData ={
+      title: this.post.title,
+      content: this.post.content
+    }
+    const id = this.route.snapshot.paramMap.get('id')
+    this.postService.update(id, formData)
+    this.editing = false
+  }
+
   delete() {
     const id = this.route.snapshot.paramMap.get('id')
     this.postService.delete(id)
